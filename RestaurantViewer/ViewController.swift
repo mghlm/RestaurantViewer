2a5abd96c3df81165fb05e12550af4e32d4e8436
Magnus Holm <mail@magnusholm.com>
2018-03-10 15:33:05 +0000

    Initial commit

---
+++ RestaurantViewer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RestaurantViewer
+//
+//  Created by Magnus Holm on 10/03/2018.
+//  Copyright © 2018 Magnus Holm. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
